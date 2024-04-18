fed8922731058c76ea53c4c68efaa2eec2e79de2
frashid17 <frashid274@gmail.com>
2024-04-18 21:51:57 +0300

    Add project files.

---
+++ firstC#/2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Console.WriteLine("ENTER FIRST NUMBER: ");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("ENTER SECOND NUMBER: ");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("ENTER THE OPERATOR: ");
+        char op = Console.ReadLine()[0];
+
+        int res = 0;
+
+        if (op == '+')
+        {
+            res = n1 + n2;
+        }
+        else if (op == '-')
+        {
+            res = n1 - n2;
+        }
+        else if (op == '*')
+        {
+            res = n1 * n2;
+        }
+        else
+        {
+            res = 0;
+        }
+        Console.WriteLine("RESULT: " + res);
+    }
+}
